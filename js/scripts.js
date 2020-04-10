c2ca8b93fd3354973505e1c30c823013d25f7102
josh <jhell85@gmail.com>
2020-04-10 12:00:57 -0700

    added constructor for order and pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+//----backEnd-----
+function Order() {
+  this.items = [];
+  this.currentId = 0;
+}
+
+function Pizza(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+  this.price = 0;
+}
+//----UI-----
 $(document).ready(function() {
   $("#pizza").on("click", function(){
     console.log(`we got a pizza`);
