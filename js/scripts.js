640e0725d85d0cec44c00c3fdc85d0e37126682e
josh <jhell85@gmail.com>
2020-04-10 15:25:57 -0700

    added listeners for document

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 function Order() {
   this.items = [];
   this.currentId = 0;
+  this.orderTotal = 0;
 }
 
 function Pizza(size, toppings) {
@@ -9,13 +10,33 @@ function Pizza(size, toppings) {
   this.toppings = toppings;
   this.price = 0;
 }
+// Pizza.prototype.getPrice(size, toppings){
+   
+// }
 //----UI-----
+let order = new Order();
+function attachContactListeners()
+
 $(document).ready(function() {
+  let size;
+  let toppings = [];
+  attachContactListeners()
+
   $("#pizza").on("click", function(){
     showPizza()
+  });
+  $("#addPie").submit(function(event) {
+    event.preventDefault();
+    $(".toppings:checked").each(function() {
+     toppings.push($(this).val());
+    })
+
   })
+
 });
 
 function showPizza(){
   $("#pizza_container").removeClass("hidden")
 }
+
+
